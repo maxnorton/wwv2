04ee369312f5c5e30e61903c5a0ec48bcd1175b4
Max Norton <maxbnorton@gmail.com>
2015-06-14 22:25:02 -0700

    unify desktop and mobile top-bar

--- parts/nav-top-offcanvas.php
+++ parts/nav-top-offcanvas.php
@@ -1,18 +1,21 @@
-<div class="sticky show-for-medium-up contain-to-grid">
-	<nav class="top-bar" data-topbar>
-		<ul class="title-area">
+<div class="contain-to-grid">
+	<nav class="top-bar row" data-topbar>
+		<ul class="title-area small-6 columns">
 			<!-- Title Area -->
 			<li class="name">
 				<h1> <a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 			</li>
 		</ul>		
-		<section class="top-bar-section right">
-			<?php joints_top_nav(); ?>
+		<section class="top-bar-section right phone small-6 columns">
+			<h4><a href="tel:+18004007238" class=""><i class="fi-telephone"></i> Speak with an expert: 800-400-7238</a></h4>
 		</section>
+		<!--section class="top-bar-section right">
+			<?php joints_top_nav(); ?>
+		</section-->
 	</nav>
 </div>
 
-<div class="show-for-small-only">
+<!--div class="show-for-small-only">
 	<nav class="tab-bar">
 		<section class="middle tab-bar-section">
 			<h1 class="title"><?php bloginfo('name'); ?></h1>
@@ -30,4 +33,4 @@
 	</ul>
 </aside>
 			
-<a class="exit-off-canvas"></a>
+<a class="exit-off-canvas"></a-->
