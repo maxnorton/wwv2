5939433fe5e1805ab4883823744af58418914892
Max Norton <maxbnorton@gmail.com>
2015-06-23 18:05:52 -0700

    make header responsive to small and medium widths

--- parts/nav-top-offcanvas.php
+++ parts/nav-top-offcanvas.php
@@ -1,4 +1,4 @@
-<div class="sticky contain-to-grid">
+<div class="sticky contain-to-grid show-for-large-up">
 	<nav class="top-bar" data-topbar>
 		<ul class="title-area">
 			<div class="slide-title-wrapper left">
@@ -14,3 +14,31 @@
 				<a class="button" href="tel:+18004007238"><i class="fi-telephone"></i>&nbsp;&nbsp;&nbsp;800-400-7238</a>
 			</li>
 		</ul>
+	</nav>
+</div>
+
+<div class="sticky hide-for-large-up">
+	<nav class="tab-bar">
+		<section class="middle tab-bar-section">
+			<div class="slide-title-wrapper left">
+				<a class="left" href="<?php echo home_url(); ?>" rel="nofollow"><img src="/wp-content/themes/wwv2/assets/images/wwv-logo-shimmery.png" /></a><div class="slide-title left" data-top="transform: translateX(0%); opacity: 1;" data-600="transform: translateX(-100%); opacity: 0;"><h1 class="text-center"><a href="<?php echo home_url(); ?>" rel="nofollow"> Whitewater Voyages</a></div>
+				<div class="social-bar hide-for-small-only"><img src="/wp-content/themes/wwv2/assets/images/social-placeholder.png" class="social-placeholder" data-top="opacity: 0;" data-500="opacity: 0;" data-700="opacity: 1;" /></div>
+			</div>
+			<div class="phone text-center show-for-small-only" data-top="opacity: 0;" data-500="opacity: 0;" data-700="opacity: 1;"><a class="button" href="tel:+18004007238"><i class="fi-telephone"></i>&nbsp;&nbsp;&nbsp;800-400-7238</a></div>
+		</section>
+		<section class="right">
+			<a href="#" class="right-off-canvas-toggle menu-icon" ><span></span></a>
+		</section>
+		<div class="phone text-center right show-for-medium-only"><a class="button" href="tel:+18004007238"><i class="fi-telephone"></i>&nbsp;&nbsp;&nbsp;800-400-7238</a></div>
+	</nav>
+</div>
+						
+<aside class="right-off-canvas-menu hide-for-large-up">
+	<ul class="off-canvas-list">
+		<li><label>Navigation</label></li>
+			<?php joints_off_canvas(); ?>    
+		<li div class="social-bar show-for-small-only"><img src="/wp-content/themes/wwv2/assets/images/social-placeholder.png" class="social-placeholder" /></li>
+	</ul>
+</aside>
+			
+<a class="exit-off-canvas"></a>
